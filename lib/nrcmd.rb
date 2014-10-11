aeb0914eecf5e6f92f646b0e46a08e97fdc838a3
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-10-12 03:07:48 +0900

    add config.rb

--- lib/nrcmd.rb
+++ lib/nrcmd.rb
@@ -2,5 +2,4 @@ require "nrcmd/version"
 require "nrcmd/cli"
 
 module Nrcmd
-  # Your code goes here...
 end
--- lib/nrcmd/cli.rb
+++ lib/nrcmd/cli.rb
@@ -4,6 +4,9 @@ require 'uri'
 require 'json'
 
 module Nrcmd
+
+  autoload :Config, 'nrcmd/config'
+
   class CLI < Thor
     class_option :config, :type => :string
     class_option :verbose, :type => :boolean
@@ -12,7 +15,7 @@ module Nrcmd
 
     def initialize(*args)
       super
-      get_conf !!options["config"] ? options["config"] : "#{Dir.pwd}/config.rb"
+      @conf = Nrcmd::Config.load(!!options["config"] ? options["config"] : "#{Dir.pwd}/config.rb")
     end
 
     desc "nrcmd help", ""
@@ -20,9 +23,9 @@ module Nrcmd
       p "no help ..."
     end
 
-    desc "nrcmd list_appid", ""
-    def list_appid
-      uri = URI.parse('https://api.newrelic.com/v2/applications.json')
+    desc "nrcmd list_appids", ""
+    def list_appids
+      uri = URI.parse('https://' + 'api.newrelic.com' + '/v2' + '/applications.json')
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
       header = { "X-Api-Key" => @conf[:nr_api_key]}
@@ -35,11 +38,6 @@ module Nrcmd
       else
         p "OMG!! #{res.code} #{res.message}"
       end
-    end
-
-    private
-    def get_conf(conf_path)
-      @conf = eval File.read "#{conf_path}"
     end
   end
 end
