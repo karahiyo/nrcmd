8d0b3827817c08752b8c929dbb844747be0e4c8c
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-10-12 21:59:32 +0900

    :waxing_crescent_moon:

--- lib/nrcmd/cli.rb
+++ lib/nrcmd/cli.rb
@@ -24,6 +24,10 @@ module Nrcmd
       Nrcmd.log_level = (!!options["verbose"] ? "DEBUG" : "INFO")
     end
 
+    #
+    #= Application
+    #
+
     desc "list_apps", "list your applications"
     long_desc <<-LONGDESC
     with --filter, -f option, filtering applications by `name`, `ids`, `language`.
@@ -31,35 +35,16 @@ module Nrcmd
     https://rpm.newrelic.com/api/explore/applications/list
     LONGDESC
     option :filter, :type => :string, :aliases => '-f', :default => ""
-      def list_apps()
-        uri = URL + '/applications.json'
-        filter_param = ""
-        options["filter"].gsub(" ", "").split(',').each do |filter|
-          fkv = filter.split('=')
-          filter_param << "filter[#{fkv[0]}]=#{fkv[1]}&"
-        end
-        res = Nrcmd::Http.get(uri, {}, filter_param)
-        result = JSON.parse(res.body)
-        print JSON[ result["applications"] ]
-    end
-
-    desc "list_servers", "list your servers."
-    long_desc <<-LONGDESC
-    with --filter, -f option, filtering applications by `name`, `ids`, `labels`.
-
-    https://rpm.newrelic.com/api/explore/servers/list
-    LONGDESC
-    option :filter, :type => :string, :aliases => '-f', :default => ""
-      def list_servers
-        uri = URL + '/servers.json'
-        filter_param = ""
-        options["filter"].gsub(" ", "").split(',').each do |filter|
-          fkv = filter.split('=')
-          filter_param << "filter[#{fkv[0]}]=#{fkv[1]}&"
-        end
-        res = Nrcmd::Http.get(uri, {}, filter_param)
-        result = JSON.parse(res.body)
-        print JSON[ result["servers"] ]
+    def list_apps()
+      uri = URL + '/applications.json'
+      filter_param = ""
+      options["filter"].gsub(" ", "").split(',').each do |filter|
+        fkv = filter.split('=')
+        filter_param << "filter[#{fkv[0]}]=#{fkv[1]}&"
+      end
+      res = Nrcmd::Http.get(uri, {}, filter_param)
+      result = JSON.parse(res.body)
+      print JSON[ result["applications"] ]
     end
 
     desc "show_app <app_id>", "show summary data of a application."
@@ -73,19 +58,10 @@ module Nrcmd
       print JSON[ result["application"]]
     end
 
-    desc "show_server <server_id>", "show summary data of a server."
-    long_desc <<-LONGDESC
-    https://rpm.newrelic.com/api/explore/servers/show
-    LONGDESC
-    def show_server(server_id)
-      uri = URL + "/servers/#{server_id}.json"
-      res = Nrcmd::Http.get(uri)
-      result = JSON.parse(res.body)
-      print JSON[ result["server"]]
-    end
-
     desc "update_app <app_id> <json_param>", "update application setting."
     long_desc <<-LONGDESC
+    `$ nrcmd update_app <app_id> '{"application": {"name": "rename_app_name"}}'
+
     sample json parameter
 
     ```
@@ -114,6 +90,42 @@ module Nrcmd
       res = Nrcmd::Http.put(uri, header, data)
       result = JSON.parse(res.body)
       print JSON[ result ]
+    end
+
+
+
+    #
+    #= Server
+    #
+
+    desc "list_servers", "list your servers."
+    long_desc <<-LONGDESC
+    with --filter, -f option, filtering applications by `name`, `ids`, `labels`.
+
+    https://rpm.newrelic.com/api/explore/servers/list
+    LONGDESC
+    option :filter, :type => :string, :aliases => '-f', :default => ""
+    def list_servers
+      uri = URL + '/servers.json'
+      filter_param = ""
+      options["filter"].gsub(" ", "").split(',').each do |filter|
+        fkv = filter.split('=')
+        filter_param << "filter[#{fkv[0]}]=#{fkv[1]}&"
+      end
+      res = Nrcmd::Http.get(uri, {}, filter_param)
+      result = JSON.parse(res.body)
+      print JSON[ result["servers"] ]
+    end
+
+    desc "show_server <server_id>", "show summary data of a server."
+    long_desc <<-LONGDESC
+    https://rpm.newrelic.com/api/explore/servers/show
+    LONGDESC
+    def show_server(server_id)
+      uri = URL + "/servers/#{server_id}.json"
+      res = Nrcmd::Http.get(uri)
+      result = JSON.parse(res.body)
+      print JSON[ result["server"]]
     end
   end
 end
