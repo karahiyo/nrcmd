65ce30c5aeee3b06cb85f30418d816337134e546
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-10-13 03:38:53 +0900

    added Application Hosts services sub-command.

--- lib/nrcmd/cli.rb
+++ lib/nrcmd/cli.rb
@@ -4,7 +4,8 @@ require 'active_support/core_ext/module'
 module Nrcmd
 
   autoload :Apps,       'nrcmd/apps_cli'
-  autoload :Server,    'nrcmd/server_cli'
+  autoload :Server,     'nrcmd/server_cli'
+  autoload :Hosts,      'nrcmd/hosts_cli'
   autoload :Config,     'nrcmd/config'
   autoload :Http,       'nrcmd/http'
   autoload :JSON,       'json'
@@ -19,15 +20,15 @@ module Nrcmd
     class_option :config, :type => :string, :aliases => "-c"
     class_option :verbose, :type => :boolean, :aliases => "-v"
 
+    register(Apps, 'apps', 'apps <sub-command>', 'sub-commands for Applications services')
+    register(Server, 'server', 'server <sub-command>', 'sub-commands for Servers services')
+    register(Hosts, 'hosts', 'hosts <sub-command>', 'sub-commands for Application Hosts services')
+
     default_command :help
     def initialize(*args)
       super
       Nrcmd.conf = Nrcmd::Config.load(!!options["config"] ? options["config"] : "#{Dir.pwd}/config.rb")
       Nrcmd.log_level = (!!options["verbose"] ? "DEBUG" : "INFO")
     end
-
-    register(Apps, 'apps', 'apps <sub-command>', 'sub-commands for Applications services')
-    register(Server, 'server', 'server <sub-command>', 'sub-commands for Servers services')
-
   end
 end
---
+++ lib/nrcmd/hosts_cli.rb
@@ -0,0 +1,42 @@
+require 'thor'
+
+module Nrcmd
+  class Hosts < Thor
+    namespace :hosts
+
+    desc "list", "list your hosts associated with the given app_id."
+    long_desc <<-LONGDESC
+    This API endpoint returns a paginated list of hosts associated with the given application.
+    Application hosts can be filtered by hostname, or the list of application host IDs.
+
+    with --filter, -f option, filtering applications by `hostname`, `ids`.
+
+    https://rpm.newrelic.com/api/explore/application_hosts/list
+    LONGDESC
+    option :filter, :type => :string, :aliases => '-f', :default => ""
+    def list(app_id)
+      uri = URL + "/applications/#{app_id}/hosts.json"
+      filter_param = ""
+      options["filter"].gsub(" ", "").split(',').each do |filter|
+        fkv = filter.split('=')
+        filter_param << "filter[#{fkv[0]}]=#{fkv[1]}&"
+      end
+      res = Nrcmd::Http.get(uri, {}, filter_param)
+      result = JSON.parse(res.body)
+      print JSON[ result["application_host"] ]
+    end
+
+    desc "show <hosts>", "show a single application host, identified by its ID."
+    long_desc <<-LONGDESC
+    https://rpm.newrelic.com/api/explore/application_hosts/show
+    LONGDESC
+    def show(app_id, host_id)
+      uri = URL + "/applications/#{app_id}/hosts/#{host_id}.json"
+      res = Nrcmd::Http.get(uri)
+      result = JSON.parse(res.body)
+      print JSON[ result["application_host"]]
+    end
+
+
+  end
+end
