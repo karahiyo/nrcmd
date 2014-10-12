99ea03525366d5dd08c13d93ffaa1ed9cf3b67e5
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-10-12 14:55:06 +0900

    added set_app_name command

--- lib/nrcmd/cli.rb
+++ lib/nrcmd/cli.rb
@@ -1,5 +1,5 @@
 require 'thor'
-require 'active_support'
+require 'active_support/core_ext/module'
 
 module Nrcmd
 
@@ -8,18 +8,20 @@ module Nrcmd
   autoload :JSON,       'json'
 
   mattr_accessor :conf
+  mattr_accessor :log_level
 
   class CLI < Thor
 
     URL = 'https://api.newrelic.com/v2'
 
-    class_option :config, :type => :string, :aliases => "-c"
-    class_option :verbose, :type => :boolean, :aliases => "-v"
+    class_option :config, :type => :string, :aliases => "c"
+    class_option :verbose, :type => :boolean, :aliases => "v"
 
     default_command :help
     def initialize(*args)
       super
       Nrcmd.conf = Nrcmd::Config.load(!!options["config"] ? options["config"] : "#{Dir.pwd}/config.rb")
+      Nrcmd.log_level = (!!options["verbose"] ? "DEBUG" : "INFO")
     end
 
     desc "nrcmd help", ""
@@ -41,6 +43,16 @@ module Nrcmd
       res = Nrcmd::Http.get(uri)
       result = JSON.parse(res.body)
       print JSON[ result["servers"] ]
+    end
+
+    desc "nrcmd set_app_name <app_id> <app_name>", ""
+    def set_app_name(app_id, app_name)
+      uri = URL + "/applications/#{app_id}.json"
+      header = { 'Content-Type' => 'application/json' }
+      data = JSON[{ "application" => { "name" => app_name } }]
+      res = Nrcmd::Http.put(uri, header, data)
+      result = JSON.parse(res.body)
+      print JSON[ result ]
     end
   end
 end
--- lib/nrcmd/http.rb
+++ lib/nrcmd/http.rb
@@ -2,12 +2,15 @@ require 'active_support'
 
 module Nrcmd
   class Http
+
+    @header = { "X-Api-Key" => Nrcmd.conf[:nr_api_key] }
+
     class << self
-      def get(uri_str)
+      def get(uri_str, _header={})
         uri = URI.parse(uri_str)
         https = Net::HTTP.new(uri.host, uri.port)
         https.use_ssl = true
-        header = { "X-Api-Key" => Nrcmd.conf[:nr_api_key]}
+        header = @header.merge _header
         res = https.start {
           https.get(uri.request_uri, header)
         }
@@ -15,6 +18,26 @@ module Nrcmd
           return res
         else
           p "OMG!! #{res.code} #{res.message}"
+          exit
+        end
+      end
+
+      def put(uri_str, _header={}, param="")
+        uri = URI.parse(uri_str)
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+        https.set_debug_output $stderr if Nrcmd.log_level == "DEBUG"
+        header = @header.merge _header
+        req = Net::HTTP::Put.new(uri.request_uri, initheader = header)
+        req.body = param
+        res = https.start {
+          https.request(req)
+        }
+        if res.code == '200'
+          return res
+        else
+          p "OMG!! #{res.code} #{res.message}"
+          exit
         end
       end
     end
