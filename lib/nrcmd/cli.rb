f219a306632f38dc8f1f01ad414103e5d39dc325
Yusuke Shimizu <a.ryuklnm@gmail.com>
2014-10-13 17:06:17 +0900

    add configure command

--- lib/nrcmd/cli.rb
+++ lib/nrcmd/cli.rb
@@ -9,6 +9,7 @@ module Nrcmd
   autoload :Config,     'nrcmd/config'
   autoload :Http,       'nrcmd/http'
   autoload :JSON,       'json'
+  autoload :PP,         'pp'
 
   mattr_accessor :conf
   mattr_accessor :log_level
@@ -30,5 +31,14 @@ module Nrcmd
       Nrcmd.conf = Nrcmd::Config.load(!!options["config"] ? options["config"] : "#{Dir.pwd}/nrcmd_config.rb")
       Nrcmd.log_level = (!!options["verbose"] ? "DEBUG" : "INFO")
     end
+
+    desc "configure", "setup your config file."
+    def configure
+      your_api_key = ask("Your NewRelic Api Key: ", :echo => false)
+      conf = eval File.read "#{Dir.pwd}/nrcmd_config.rb"
+      conf.merge! Hash[ :nr_api_key => your_api_key ]
+      File.write("#{Dir.home}/.nrcmd_config.rb", PP.pp(conf, ''))
+    end
+
   end
 end
